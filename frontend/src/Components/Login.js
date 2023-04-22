30a6277479f53f8efc4333d5dfaa96b1931d9300
Tanuththiga <anuththiga25jt@gmail.com>
2023-04-22 15:07:43 +0800

    login component

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,17 @@
+function Login() {
+  return (
+    <div className="col-3 offset-4">
+        <h1>Login</h1>
+        
+        <input type="text" className="form-control mb-3" placeholder="Email"/>
+        <input type="password" className="form-control mb-3" placeholder="Password"/>
+        <button className="btn btn-primary">Login</button>
+
+
+
+        
+    </div>
+  );
+}
+
+export default Login
