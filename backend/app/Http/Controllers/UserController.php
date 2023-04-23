c0d70faca784a0e180088396fa90d5f053bc27c4
Tanuththiga <anuththiga25jt@gmail.com>
2023-04-24 03:59:37 +0800

    hardcoded the data

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -8,14 +8,47 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    
+       
     function login(Request $req)
     {
-        $user = User::where('email', $req->email)->first();
-        if(!$user || !Hash::check($req->password,$user->password))
+        // $user = User::where('email', $req->email)->first();
+        $users = [
+            [ 
+                'email' => 'Admin@gmail.com',
+                'password' => 'test123',
+                'created_at' => '2023-04-21T17:24:14.000000Z',
+                'updated_at' => '2023-04-21T17:24:14.000000Z' 
+            ],
+            [
+                'email' => 'User@gmail.com',
+                'password' => 'test123',
+                'created_at' => '2023-04-21T17:24:18.000000Z',
+                'updated_at' => '2023-04-21T17:24:18.000000Z'
+            ],
+            [
+                'email' => 'Test@gmail.com',
+                'password' => 'test123',
+                'created_at' => '2023-04-21T17:24:20.000000Z',
+                'updated_at' => '2023-04-21T17:24:20.000000Z' 
+            ]
+          ];
+        
+        $user = array();
+        foreach($users as $key=>$value)
+        {
+            if( $value['email'] == $req->email && $value['password'] == $req->password)
+            {
+                $user = $value;
+            }        
+        }
+
+        if(!$user['email'] || !$user['password'])
         {
             return ["error"=>"Email or Password is not correct"];
         }
-        return $user;
+        else
+        {
+            return $user;
+        } 
     }
 }
